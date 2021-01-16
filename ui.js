4c7f38be8737c6e82d23091e97f9802c1639bf1f
albgom21 <62644841+albgom21@users.noreply.github.com>
2021-01-16 16:03:39 +0100

    Cambiado nombre de animatedtext a textoanimado

--- ui.js
+++ ui.js
@@ -1,5 +1,5 @@
 import Barravida from './barravida.js';
-import AnimatedText from './animatedtext.js';
+import AnimatedText from './textoanimado.js';
 
 export default class Ui extends Phaser.Scene {  
   constructor() {
